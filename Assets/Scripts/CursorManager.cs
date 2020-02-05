6427a1b5c739d58dac78c8dceff6a4907a241c47
Jayas <34701149+jayasio@users.noreply.github.com>
2020-02-05 11:24:01 +0100

    Calibration

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -8,8 +8,10 @@ public class CursorManager : MonoBehaviour
     #region variables
     TcpServer tcp = new TcpServer();
     Tracker tracker = new Tracker();
-    float zCache = 3;
-    bool zCacheSet = false;
+    // bool zCacheSet = false;
+    float threshold = 0.004f;
+    float speedFactor = 20;
+    float zCache = 0.5f;
     #endregion
 
     public bool isConnected() => tcp.isConnected();
@@ -20,28 +22,28 @@ public class CursorManager : MonoBehaviour
     {
         if (tcp.isConnected() && tcp.stream.Count != 0)
         {
-            if (!zCacheSet)
-            {
-                // zCache = (float)Math.Round(tracker.position.z, 1);
-                zCache = 3;
-                zCacheSet = true;
-            }
+            // if (!zCacheSet)
+            // {
+            //     // zCache = (float)Math.Round(tracker.position.z, 1);
+            //     zCache = 0.7f;
+            //     zCacheSet = true;
+            // }
 
             tracker = JsonUtility.FromJson<Tracker>(tcp.stream.Dequeue());
 
-            if (Math.Abs(tracker.position.x) > 0.4)
-                transform.Translate(Time.deltaTime * 3 * (float)Math.Round(tracker.position.x, 1), 0, 0, Space.Self);
+            if (Math.Abs(tracker.position.x) > threshold)
+                transform.Translate(-1 * Time.deltaTime * speedFactor * (float)Math.Round(tracker.position.x, 1), 0, 0, Space.Self);
 
-            if (Math.Abs(tracker.position.y) > 0.4)
-                transform.Translate(0, Time.deltaTime * 3 * (float)Math.Round(tracker.position.y, 1), 0, Space.Self);
+            if (Math.Abs(tracker.position.y) > threshold)
+                transform.Translate(0, Time.deltaTime * speedFactor * (float)Math.Round(tracker.position.y, 1), 0, Space.Self);
 
-            if (Math.Abs(tracker.position.z - zCache) > 0.4)
-                transform.Translate(0, 0, Time.deltaTime * 3 * ((float)Math.Round(tracker.position.z, 1) - zCache), Space.Self);
+            if (Math.Abs(tracker.position.z - zCache) > threshold)
+                transform.Translate(0, 0, -1 * Time.deltaTime * speedFactor * ((float)Math.Round(tracker.position.z, 1) - zCache), Space.Self);
 
-            if (Math.Abs(tracker.position.x) > 0.4 && Math.Abs(tracker.position.y) > 0.4 && Math.Abs(tracker.position.z) > 0.4)
+            if (Math.Abs(tracker.position.x) > threshold && Math.Abs(tracker.position.y) > threshold && Math.Abs(tracker.position.z) > threshold)
                 transform.GetComponent<Renderer>().material.color = Color.white;
-            else if (tracker.position.z - zCache > 0.4) transform.GetComponent<Renderer>().material.color = Color.red;
-            else if (tracker.position.z - zCache < -0.4) transform.GetComponent<Renderer>().material.color = Color.blue;
+            else if (tracker.position.z - zCache > threshold) transform.GetComponent<Renderer>().material.color = Color.red;
+            else if (tracker.position.z - zCache < -1 * threshold) transform.GetComponent<Renderer>().material.color = Color.blue;
             else transform.GetComponent<Renderer>().material.color = Color.yellow;
 
             // transform.Rotate(
@@ -57,8 +59,8 @@ public class CursorManager : MonoBehaviour
 
     void OnGUI()
     {
-        if (Math.Abs(tracker.position.x) > 0.4) GUI.Label(new Rect(100, 100, 100, 100), "X moving " + Math.Round(tracker.position.x, 1));
-        if (Math.Abs(tracker.position.y) > 0.4) GUI.Label(new Rect(100, 150, 100, 100), "Y moving " + Math.Round(tracker.position.y, 1));
-        if (Math.Abs(tracker.position.z - zCache) > 0.4) GUI.Label(new Rect(100, 200, 100, 100), "Z moving " + Math.Round(tracker.position.z - zCache, 1));
+        if (Math.Abs(tracker.position.x) > threshold) GUI.Label(new Rect(100, 100, 100, 100), "X moving " + Math.Round(tracker.position.x, 1));
+        if (Math.Abs(tracker.position.y) > threshold) GUI.Label(new Rect(100, 150, 100, 100), "Y moving " + Math.Round(tracker.position.y, 1));
+        if (Math.Abs(tracker.position.z - zCache) > threshold) GUI.Label(new Rect(100, 200, 100, 100), "Z moving " + Math.Round(tracker.position.z - zCache, 1));
     }
 }
